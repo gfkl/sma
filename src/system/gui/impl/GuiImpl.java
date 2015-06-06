1e7a91f9ea19321597065db22e9da129d041baf0
Houpert <xxdiagammaxx@yahoo.fr>
2015-06-06 10:37:38 +0200

    Add menu for speedUp/Down/pause

--- src/system/gui/impl/GuiImpl.java
+++ src/system/gui/impl/GuiImpl.java
@@ -1,94 +1,132 @@
 package system.gui.impl;
 
-import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JTable;
-import javax.swing.table.AbstractTableModel;
+import javax.swing.KeyStroke;
 
 import system.dto.EnvConfigDTO;
 import system.dto.EnvObsDTO;
 import system.gui.GuiComponent;
 import system.gui.interfaces.IGui;
-import system.model.objects.Agent;
-import system.model.objects.Box;
-import system.model.objects.Grid;
-import system.model.objects.Nest;
 
 public class GuiImpl extends GuiComponent {
 
 	private JFrame frame;
-
+	private final int incSpeed = 250;
+	private int speed;
+	private int currentSpeed;
+	
 	@Override
 	protected IGui make_printer() {
 		return new IGui() {
-
 			@Override
 			public EnvConfigDTO printEnv(EnvObsDTO env) {
 				Object[][] objs = (Object[][]) env.getGrid().getGrid();
-				System.out.println("LENGTH : " + objs.length);
 				FieldView field = new FieldView(objs);
 				JTable table = new JTable(field);
 				table.setDefaultRenderer(Object.class, new MonCellRenderer());
 				frame.getContentPane().removeAll();
 				frame.getContentPane().add(table);
 				frame.setVisible(true);
-				// afficher a chaque appel de la méthode
-				// TODO Peut influer sur l'env via les params de config
-				return new EnvConfigDTO(100);
+				return new EnvConfigDTO(speed);
 			}
 
 			@Override
 			public void createGUI() {
-				// TODO Creer la GUI
+				speed = 100;
 				frame = new JFrame();
 				frame.setTitle("SMA-AL Viewer");
 				frame.setBounds(100, 100, 900, 900);
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				JButton button = new JButton();
-				button.setText("Pause");
-				frame.getContentPane().add(button);
+				
+				createMenuBar();
 			}
+
+			
+		    private void createMenuBar() {
+
+		        JMenuBar menubar = new JMenuBar();
+
+		        JMenu file = new JMenu("Action");
+		        file.setMnemonic(KeyEvent.VK_F);
+
+		        
+		        
+		        JMenuItem eMenuItemSpeedUp = new JMenuItem("Speed Up");
+		        eMenuItemSpeedUp.setAccelerator(KeyStroke.getKeyStroke(
+		                java.awt.event.KeyEvent.VK_UP, 
+		                java.awt.Event.CTRL_MASK));
+		        eMenuItemSpeedUp.setToolTipText("Pause application");
+		        eMenuItemSpeedUp.addActionListener(new ActionListener() {
+		            @Override
+		            public void actionPerformed(ActionEvent event) {
+		            	if(speed > incSpeed)
+		            		speed -= incSpeed;
+		            	else
+		            		speed = 0;
+
+		            }
+		        });
+		        
+		        JMenuItem eMenuItemSpeedDown = new JMenuItem("Speed Down");
+		        eMenuItemSpeedDown.setAccelerator(KeyStroke.getKeyStroke(
+		                java.awt.event.KeyEvent.VK_DOWN, 
+		                java.awt.Event.CTRL_MASK));
+		        eMenuItemSpeedDown.setToolTipText("Pause application");
+		        eMenuItemSpeedDown.addActionListener(new ActionListener() {
+		            @Override
+		            public void actionPerformed(ActionEvent event) {
+		            		speed += incSpeed;
+		            }
+		        });
+		        
+		        
+		        
+		        JMenuItem eMenuItemPause = new JMenuItem("Pause");
+		        eMenuItemPause.setAccelerator(KeyStroke.getKeyStroke(
+		                java.awt.event.KeyEvent.VK_P, 
+		                java.awt.Event.CTRL_MASK));
+		        eMenuItemPause.setToolTipText("Pause application");
+		        eMenuItemPause.addActionListener(new ActionListener() {
+		            @Override
+		            public void actionPerformed(ActionEvent event) {
+		            	if(speed != 0){
+		            		currentSpeed = speed;
+		            		speed = 10000;
+		            	}else
+		            		speed = currentSpeed;
+		            }
+		        });
+		        
+		        
+		        JMenuItem eMenuItemExit = new JMenuItem("Exit");
+		        eMenuItemExit.setAccelerator(KeyStroke.getKeyStroke(
+		                java.awt.event.KeyEvent.VK_E, 
+		                java.awt.Event.CTRL_MASK));
+		        eMenuItemExit.setToolTipText("Exit application");
+		        eMenuItemExit.addActionListener(new ActionListener() {
+		            @Override
+		            public void actionPerformed(ActionEvent event) {
+		                System.exit(0);
+		            }
+		        });
+
+		        file.add(eMenuItemSpeedUp);
+		        file.add(eMenuItemSpeedDown);
+		        file.add(eMenuItemPause);
+		        file.add(eMenuItemExit);
+		        menubar.add(file);
+
+		        frame.setJMenuBar(menubar);
+		    }
 		};
+
 	}
-
-
-//	public class FieldView extends AbstractTableModel {
-//
-//		private Object[][] data;
-//
-//
-//		@Override
-//		public int getRowCount() {
-//			return 50;
-//		}
-//
-//		@Override
-//		public Object getValueAt(int rowIndex, int columnIndex) {
-//
-//			System.out.println("AGENT");
-//			System.out.println("AGENT");
-//			System.out.println("AGENT");
-//			System.out.println("AGENT");
-//			System.out.println("AGENT");
-//			Object comp = data[rowIndex][columnIndex];
-//
-//			if (comp instanceof Agent) {
-//				return "A";
-//			} else if (comp instanceof Box) {
-//				return "B";
-//			} else if (comp instanceof Grid) {
-//				return "";
-//			} else if (comp instanceof Nest) {
-//				return "N";
-//			} else
-//				return "";
-//
-//		}
-//
-//		@Override
-//		public int getColumnCount() {
-//			return 50;
-//		}
-//	}
-
 }
